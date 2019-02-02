404081b7acec53a40686e3fb0170e2d0afd52052
yezhu <1501905755@qq.com>
2019-02-02 08:28:11 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    //protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -26,4 +28,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
